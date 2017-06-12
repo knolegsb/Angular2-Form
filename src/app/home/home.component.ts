2968cf29e5752fc47f9048e1f4e31cede8b4eec9
Sean <seon2you@hotmail.com>
2017-06-11 20:04:04 -0700

    intial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import { Employee } from '../models/employee.model';
+
+@Component({
+  selector: 'home',
+  styleUrls: ['./home.component.css'],
+  templateUrl: './home.component.html'
+})
+export class HomeComponent {
+  //languages : string[];
+  
+  languages = ['English', 'Spanish', 'Other'];
+  model = new Employee('Darla', 'Smith', true, 'w2', 'English');
+
+  firstNameToUpperCase(value: string){
+    if (value.length > 0){
+      this.model.firstName = value.charAt(0).toUpperCase() + value.slice(1);
+    }
+    else{
+      this.model.firstName = value;
+    }
+  }
+}
